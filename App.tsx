872fc4fa10f88cdbf43dfd4c432e5b6aad8b82ee
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2023-09-18 10:58:55 +0530

    implemented navigation flow

--- App.tsx
+++ App.tsx
@@ -1,45 +1,24 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Homescreen from './src/screens/Homescreen';
-import Signup from './src/components/Authentication/Signup';
-import Signin from './src/components/Authentication/Signin';
-import store from './src/redux/store';
-import Authentication from './src/navigation';
+import React from "react";
+import { Provider } from "react-redux";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Homescreen from "./src/screens/Homescreen";
+import Signup from "./src/screens/Signup";
+import Signin from "./src/components/Authentication/Signin";
+import store from "./src/redux/store";
+import Authentication from "./src/navigation";
+import { AppNavigator } from "./src/navigation/AppNavigator";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView style={{flex: 1}}>
-        <Authentication />
-        {/* <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen
-              name="Authentication"
-              component={Authentication}
-              options={{headerShown: false}}
-            />
-            {/* <Stack.Screen
-              name="Signin"
-              component={Signin}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="Signup"
-              component={Signup}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="Home"
-              component={Homescreen}
-              options={{headerShown: false}}
-            />
-          </Stack.Navigator>
-        </NavigationContainer> */}
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
       </SafeAreaView>
     </Provider>
   );
@@ -47,8 +26,8 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
 });
 
