ccabe85fbf8a98a38824a035a5fae686f8710d14
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2023-10-23 17:00:28 +0530

    fix for event not found UI

--- src/components/Event/Events.tsx
+++ src/components/Event/Events.tsx
@@ -7,10 +7,43 @@ import {
   ActivityIndicator,
 } from "react-native";
 import EventCard from "./EventCard";
+import { MONTHS } from "../../constants/constants";
+import { useSelector } from "react-redux";
 
-const Events = ({ date = "", cards, isLast = false, loading = "false" }) => {
+const Events = ({
+  date = "",
+  cards,
+  displayNotFound = false,
+  isLast = false,
+  isFirst = false,
+  loading = "false",
+}) => {
+  const { date: calendarDate } = useSelector((state) => state.home);
+
+  const renderNotFoundSection = () => {
+    return (
+      <View>
+        <Text style={{ color: "#FFFFFF" }}>{`${
+          MONTHS[calendarDate?.split("-")[1]]?.long
+        } ${calendarDate?.split("-")[2]}, ${
+          calendarDate?.split("-")[0]
+        }`}</Text>
+        <Text
+          style={{
+            color: "#FC7A1B",
+            marginVertical: 6,
+            marginBottom: 20,
+          }}
+        >
+          No events found for the selected date
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={[styles.container, { marginBottom: isLast ? 180 : 16 }]}>
+    <View style={[styles.container, { marginBottom: isLast ? 260 : 16 }]}>
+      {displayNotFound && isFirst && renderNotFoundSection()}
       {date && <Text style={styles.date}>{date}</Text>}
       {cards.map((card, i) => (
         <EventCard key={i + 1} data={card} />
